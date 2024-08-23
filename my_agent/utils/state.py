9a50fe6b6ba094039b6a55880cacb0c8576ce20a
Alex Kira <alex.kira@gmail.com>
2024-08-23 15:48:01 -0700

    replace messages

--- my_agent/utils/state.py
+++ my_agent/utils/state.py
@@ -1,6 +1,21 @@
 from langgraph.graph import add_messages
 from langchain_core.messages import BaseMessage
-from typing import TypedDict, Annotated, Sequence
+from typing import TypedDict, Annotated, Sequence, Union
+
+from langchain_core.messages import (
+    AnyMessage,
+    MessageLikeRepresentation,
+    RemoveMessage,
+    convert_to_messages,
+    message_chunk_to_message,
+)
+Messages = Union[list[MessageLikeRepresentation], MessageLikeRepresentation]
+
+
+def replace_messages(left: Messages, right: Messages) -> Messages:
+    messages = add_messages(left, right)
+    return messages[-1:]
+
 
 class AgentState(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], add_messages]
+    messages: Annotated[Sequence[BaseMessage], replace_messages]
